d253b0a9c2e5c2f1c9b4ed5cf24d7b4fd72c8720
Ben Chambers <bchambers@clearfoundation.com>
2013-02-14 22:13:01 +0000

    - Fixed deps

--- deploy/info.php
+++ deploy/info.php
@@ -4,7 +4,7 @@
 // General information
 ///////////////////////////////////////////////////////////////////////////// 
 $app['basename'] = 'photo_organizer';
-$app['version'] = '1.0.0';
+$app['version'] = '1.0.1';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
@@ -31,7 +31,7 @@ $app['subcategory'] = lang('base_subcategory_file');
 
 $app['core_requires'] = array(
     'app-flexshare-core',
-    'exiftool'
+    'perl-Image-ExifTool'
 );
 
 $app['core_file_manifest'] = array(
