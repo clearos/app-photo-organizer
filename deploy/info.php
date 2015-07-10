465d9d8d0f9514e735635095e8d93f0c50f740ed
pcbaldwin <pbaldwin@clearfoundation.com>
2015-07-10 13:13:42 -0400

    Prepping for snapshot build

--- deploy/info.php
+++ deploy/info.php
@@ -4,7 +4,7 @@
 // General information
 ///////////////////////////////////////////////////////////////////////////// 
 $app['basename'] = 'photo_organizer';
-$app['version'] = '2.0.0';
+$app['version'] = '2.1.6';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-photo-organizer.spec
+++ packaging/app-photo-organizer.spec
@@ -1,7 +1,7 @@
 
 Name: app-photo-organizer
 Epoch: 1
-Version: 2.0.0
+Version: 2.1.6
 Release: 1%{dist}
 Summary: Photo Organizer
 License: GPLv3
